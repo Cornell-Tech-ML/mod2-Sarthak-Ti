9d50fd150551f2ac04a6f02100c2172a6b1cdcaf
Tiwari <tiwaris2@islogin01.mskcc.org>
2024-10-16 14:11:23 -0400

    fix type annotation for permute.backward

--- minitorch/tensor_functions.py
+++ minitorch/tensor_functions.py
@@ -282,7 +282,7 @@ class Permute(Function):
         return ts
 
     @staticmethod
-    def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, int, ...]:
+    def backward(ctx: Context, grad_output: Tensor) -> Tuple[Tensor, *tuple[int, ...]]:
         """Permute tensor Backward"""
         (dims,) = ctx.saved_values
         dims2 = [int(dim.item()) for dim in dims]
